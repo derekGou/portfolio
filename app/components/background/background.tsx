04c0a013b4c07639c01058ba11367c5a46bb0b69
Derek Gou <derekgou19@gmail.com>
2025-09-01 19:10:24 -0400

    Update background.tsx

--- app/components/background/background.tsx
+++ app/components/background/background.tsx
@@ -42,7 +42,7 @@ export default function Background({image}: Props) {
     }, [])
 
     return (
-        <div className="fixed top-0 left-0 w-screen h-dvh overflow-hidden -z-50">
+        <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden -z-50">
             <img
                 src={`/bg/IMG_${image}.JPG`}
                 alt="Background image"
